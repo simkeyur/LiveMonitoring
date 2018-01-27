c86015a9974248de6d8183a4215c85a4ac2742b7
Keyur Patel <simkeyur@gmail.com>
2018-01-27 04:12:54 -0600

    Added Dashboard UI Components basic

--- AngularAppStarter/Scripts/Controllers/appController.js
+++ AngularAppStarter/Scripts/Controllers/appController.js
@@ -1,5 +1,28 @@
 ﻿var myApp = angular.module('myApp', []);
 
 myApp.controller('appController', ['$scope', function ($scope) {
-    $scope.greeting = 'Hi There!';
+    $scope.greeting = 'Order Life Cycle';
+    $scope.OrderCreated = getRandomVal(150, 300);
+    $scope.OrderFullfilled = getRandomVal(150, 300);
+    $scope.ReadyForShipment = getRandomVal(150, 300);
+    $scope.OrderShipped = getRandomVal(400, 500);
+    $scope.selectedprop = { label: "1 Minute", value: 1 };
+
+    //TimerSelect
+    $scope.setSelected = function (prop) { $scope.selectedprop = prop; };
+    $scope.props = [{ label: "1 Minute", value: 1 },
+                        { label: "2 Minutes", value: 2 },
+                        { label: "5 Minutes", value: 5 },
+                        { label: "10 Minutes", value: 10 },
+                        { label: "30 Minutes", value: 30 },
+                        { label: "1 Hour", value: 60 }
+                       ];
+    
+
+
+    function getRandomVal(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 }]);
